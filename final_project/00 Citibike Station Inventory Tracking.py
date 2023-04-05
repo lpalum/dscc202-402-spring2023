06f6cc269025341d28b70bed1c5504a222475c79
lpalum@gmail.com <lpalum@gmail.com>
2023-04-05 18:32:22 +0000

    Addes the promote model widget

--- final_project/00 Citibike Station Inventory Tracking.py
+++ final_project/00 Citibike Station Inventory Tracking.py
@@ -17,44 +17,47 @@ dbutils.widgets.removeAll()
 dbutils.widgets.text('01.start_date', "2021-10-01")
 dbutils.widgets.text('02.end_date', "2023-03-01")
 dbutils.widgets.text('03.hours_to_forecast', '4')
+dbutils.widgets.text('04.promote_model', 'No')
 
 start_date = str(dbutils.widgets.get('01.start_date'))
 end_date = str(dbutils.widgets.get('02.end_date'))
 hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-print(start_date,end_date,hours_to_forecast)
+promote_model = str(dbutils.widgets.get('04.promote_model'))
+
+print(start_date,end_date,hours_to_forecast, promote_model)
 
 # COMMAND ----------
 
-# DBTITLE 1,ETL
+# DBTITLE 1,Run the ETL Notebook
 # Run the Data Prepartion note the one hour 3600 second timeout!
-result = dbutils.notebook.run("01 etl", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast})
+result = dbutils.notebook.run("01 etl", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast,"04.promote_model":promote_model})
 
 # Check the results
 assert json.loads(result)["exit_code"] == "OK", "Data Preparation Failed!" # Check to see that it worked
 
 # COMMAND ----------
 
-# DBTITLE 1,EDA
+# DBTITLE 1,Run the EDA Notebook
 # Run the Data Prepartion
-result = dbutils.notebook.run("02 eda", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast})
+result = dbutils.notebook.run("02 eda", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast,"04.promote_model":promote_model})
 
 # Check the results
 assert json.loads(result)["exit_code"] == "OK", "Data Preparation Failed!" # Check to see that it worked
 
 # COMMAND ----------
 
-# DBTITLE 1,Model Development
+# DBTITLE 1,Run Model Development Notebook
 # Run the Data Prepartion
-result = dbutils.notebook.run("03 mdl", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast})
+result = dbutils.notebook.run("03 mdl", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast,"04.promote_model":promote_model})
 
 # Check the results
 assert json.loads(result)["exit_code"] == "OK", "Data Preparation Failed!" # Check to see that it worked
 
 # COMMAND ----------
 
-# DBTITLE 1,Inventory Prediction
+# DBTITLE 1,Run Station Inventory Forecast Notebook
 # Run the Data Prepartion
-result = dbutils.notebook.run("04 app", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast})
+result = dbutils.notebook.run("04 app", 3600, {"01.start_date":start_date, "02.end_date":end_date,"03.hours_to_forecast":hours_to_forecast,"04.promote_model":promote_model})
 
 # Check the results
 assert json.loads(result)["exit_code"] == "OK", "Data Preparation Failed!" # Check to see that it worked
--- final_project/04 app.py
+++ final_project/04 app.py
@@ -7,7 +7,9 @@
 start_date = str(dbutils.widgets.get('01.start_date'))
 end_date = str(dbutils.widgets.get('02.end_date'))
 hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-print(start_date,end_date,hours_to_forecast)
+promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
+
+print(start_date,end_date,hours_to_forecast, promote_model)
 
 print("YOUR CODE HERE...")
 

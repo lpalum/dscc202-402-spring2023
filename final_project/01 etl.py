a19d41ff94aed7b279de767cc159211a57cd786a
fcolombo@u.rochester.edu <fcolombo@u.rochester.edu>
2023-04-11 07:01:37 +0000

    ..

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -3,6 +3,7 @@
 
 # COMMAND ----------
 
+
 start_date = str(dbutils.widgets.get('01.start_date'))
 end_date = str(dbutils.widgets.get('02.end_date'))
 hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
@@ -18,3 +19,7 @@ import json
 # Return Success
 dbutils.notebook.exit(json.dumps({"exit_code": "OKkkkkkkkkkkkkkkkkkkkkkkk"}))
 # comment
+
+# COMMAND ----------
+
+..

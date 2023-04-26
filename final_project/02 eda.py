051779efa9b64f61822cf71e2dc4885b0a88ddba
jrcrossett22@gmail.com <jrcrossett22@gmail.com>
2023-04-26 22:08:04 +0000

    reinserted a cell with the widget data that was on original notebook

--- final_project/02 eda.py
+++ final_project/02 eda.py
@@ -3,6 +3,15 @@
 
 # COMMAND ----------
 
+start_date = str(dbutils.widgets.get('01.start_date'))
+end_date = str(dbutils.widgets.get('02.end_date'))
+hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
+promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
+
+print(start_date,end_date,hours_to_forecast, promote_model)
+
+# COMMAND ----------
+
 #dfStatus = spark.read.load('dbfs:/FileStore/tables/G10/bronze_historic_bike.delta/')
 #display(dfStatus)
 #dbfs:/FileStore/tables/G10/bronze_historic_bike.delta/

483bf1f5ea34644c098c616011c1f69c7a4f1321
nicachist@gmail.com <nicachist@gmail.com>
2023-04-28 18:06:30 +0000

    update eda

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -4,13 +4,13 @@
 # COMMAND ----------
 
 
-start_date = str(dbutils.widgets.get('01.start_date'))
-end_date = str(dbutils.widgets.get('02.end_date'))
-hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
+#start_date = str(dbutils.widgets.get('01.start_date'))
+#end_date = str(dbutils.widgets.get('02.end_date'))
+#hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
+#promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
 
-print(start_date,end_date,hours_to_forecast, promote_model)
-print("YOUR CODE HERE...")
+#print(start_date,end_date,hours_to_forecast, promote_model)
+#print("YOUR CODE HERE...")
 
 
 # COMMAND ----------

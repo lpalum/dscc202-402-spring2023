763b31f9f2823e37f992b9205aa66405d0604a87
jrcrossett22@gmail.com <jrcrossett22@gmail.com>
2023-05-01 05:03:57 +0000

    Station Map and current timestamp

--- final_project/04 app.py
+++ final_project/04 app.py
@@ -11,11 +11,37 @@ promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower(
 
 print(start_date,end_date,hours_to_forecast, promote_model)
 
-print("YOUR CODE HERE...")
 
 # COMMAND ----------
 
-import json
+pip install folium
+
+# COMMAND ----------
+
+import folium
+from pyspark.sql.functions import *
+
+#Get and display current timestamp
+
+df = spark.sql("select to_utc_timestamp(current_timestamp(), '+04:00')")
+currentTS = df.collect()[0][0]
+displayHTML("<h2>Current Timestamp: </h2>")
+displayHTML(currentTS)
+
+# Create map of station location and header for map
+
+displayHTML("<br><br><h2>Station: 8 Ave & W 33 St</h2>")
+map=folium.Map(location=[40.751551,-73.993934], zoom_start=17, min_zoom=17, max_zoom=17)
+map.add_child(folium.Marker(location=[40.751551,-73.993934],popup='8 Ave & W 33 St',icon=folium.Icon(color='red')))
+map
+
+# COMMAND ----------
+
+|import json
 
 # Return Success
 dbutils.notebook.exit(json.dumps({"exit_code": "OK"}))
+
+# COMMAND ----------
+
+
